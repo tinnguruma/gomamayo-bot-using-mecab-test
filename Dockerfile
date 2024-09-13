48293e8cf5a2baecd45d04015b92be62ace96414
Tinguruma <90504745+tinnguruma@users.noreply.github.com>
2024-09-13 23:10:34 +0900

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,12 @@ RUN apt-get update \
   && apt-get install -y mecab-ipadic \
   && apt-get install -y libmecab-dev \
   && apt-get install -y mecab-ipadic-utf8 \
-  && apt-get install -y swig
+  && apt-get install -y swig \
+  && apt-get install tesseract-ocr
+
+# 任意の場所に学習データを配置
+RUN mkdir -p /usr/share/tesseract-ocr/4.00/tessdata
+COPY ./jpn.traineddata /usr/share/tesseract-ocr/4.00/tessdata/jpn.traineddata
 
 WORKDIR /bot
 COPY requirements.txt /bot/
--- main.py
+++ main.py
@@ -3,17 +3,26 @@ import os
 import time
 import discord
 import MeCab
+import pytesseract
+from PIL import Image
+import requests
+from io import BytesIO
 from keep_alive import keep_alive
 
 client = discord.Client(intents=discord.Intents.all())
 # client = discord.Client(intents=discord.Intents.default())
 # tagger = MeCab.Tagger()
-tagger = MeCab.Tagger('-r /etc/mecabrc')
+tagger = MeCab.Tagger("-r /etc/mecabrc")
+
+pytesseract.pytesseract.tesseract_cmd = r'path_to_your_tesseract_executable'
+tessdata_dir_config = '--tessdata-dir "/usr/share/tesseract-ocr/4.00/tessdata"'
+
 
 @client.event
 async def on_ready():
     print("log-in!")
     print(discord.__version__)
+
 
 @client.event
 async def on_message(message):
@@ -27,34 +36,55 @@ async def on_message(message):
     start_time = time.time()
     txt = message.content
     counter = 0
+    log_master = ""
+
+    # 画像判別
+    if message.attachments:
+        for attachment in message.attachments:
+            if attachment.filename.endswith(("png", "jpg", "jpeg", "gif")):
+                print("画像だよ:", attachment.url)
+
+                # 画像をダウンロードしてOCRを実行
+                response = requests.get(attachment.url)
+                img = Image.open(BytesIO(response.content))
+                ocr_text = pytesseract.image_to_string(
+                    img, lang="jpn", config=tessdata_dir_config
+                )
+
+                # OCR結果とmessage.contentを結合
+                txt = "OCR_text------ \n" + ocr_text + "------------ \n"
+    else:
+        pass
 
     if "ゴママヨ" in txt:
         if not txt.startswith("ゴママヨ！？["):
             await message.channel.send("ゴママヨ！？")
-            
+
             end_time = time.time()
-            elapsed_time = round(end_time - start_time,5)
+            elapsed_time = round(end_time - start_time, 5)
             await message.channel.send(f"処理時間: {elapsed_time}秒")
 
     if txt:
-        log_master = []
-        
-        if re.search(r'[A-z]', txt) is not None:
-            txt = re.sub(r'[A-z]', '', txt)
-            log_master.append("英語は省かれました")
-        if re.search(r'[0-9]', txt) is not None:
-            txt = re.sub(r'[0-9]', '', txt)
-            log_master.append("数字は省かれました。")
-        if re.search(r'[-/:-@[-´{-~！？!?\.、\(\)\（\）。・\n…]', txt) is not None:
-            txt = re.sub(r'[ -/:-@[-´{-~！？!?\.、。・\n…]', '', txt)
-            log_master.append("一部記号が省かれました。")
-        
+
+        if re.search(r"[A-z]", txt) is not None:
+            txt = re.sub(r"[A-z]", "", txt)
+            log_master += "\n" + "英語は省かれました"
+        if re.search(r"[0-9]", txt) is not None:
+            txt = re.sub(r"[0-9]", "", txt)
+            log_master += "\n" + "数字は省かれました。"
+        if re.search(r"[-/:-@[-´{-~！？!?\.、\(\)\（\）。・\n…]", txt) is not None:
+            txt = re.sub(r"[ -/:-@[-´{-~！？!?\.、。・\n…]", "", txt)
+            log_master += "\n" + "一部記号が省かれました。"
+
         arr = tagger.parse(txt).splitlines()
         beforeSound, beforeSound2 = 0, 0
-        
+
+        log_master += "\n"
+
         for item in arr:
             try:
                 sound = str(item.split(",")[-2])
+                log_master += sound + "/"
                 if beforeSound != 0 and beforeSound == sound[0]:
                     print("ゴママヨ！？[" + sound[0])
                     await message.channel.send("ゴママヨ！？[" + sound[0] + "]")
@@ -69,9 +99,9 @@ async def on_message(message):
                 pass
         if counter > 0:
             end_time = time.time()
-            elapsed_time = round(end_time - start_time,5)
+            elapsed_time = round(end_time - start_time, 5)
             await message.channel.send(f"処理時間: {elapsed_time}秒")
-        
+
         if txt.startswith("log"):
             await message.channel.send(log_master)
 
